a8e88f66320293afe72ce9ba1f61968e69e4bb4d
bingoo <bingoo.huang@gmail.com>
2020-02-17 10:50:12 +0800

    refactor code

--- model/model.go
+++ model/model.go
@@ -3,7 +3,6 @@ package model
 import (
 	"fmt"
 	"net"
-	"time"
 
 	"github.com/bingoohuang/hraftd/util"
 	"github.com/hashicorp/raft"
@@ -47,48 +46,6 @@ type Rsp struct {
 	OK   bool        `json:"ok"`
 	Msg  string      `json:"msg,omitempty"`
 	Data interface{} `json:"data,omitempty"`
-}
-
-// Store is the interface Raft-backed key-value stores must implement.
-type Store interface {
-	// Get returns the value for the given key.
-	Get(key string) (string, bool)
-
-	// IsLeader tells the current node is raft leader or not.
-	IsLeader() bool
-
-	// Set sets the value for the given key, via distributed consensus.
-	Set(key, value string) error
-
-	// Delete removes the given key, via distributed consensus.
-	Delete(key string) error
-
-	// Join joins the node, identified by nodeID and reachable at addr, to the cluster.
-	Join(nodeID string, addr string) error
-
-	// Remove removes node from the cluster
-	Remove(nodeID string) error
-
-	// RaftStats returns the raft stats
-	RaftStats() map[string]interface{}
-
-	// Cluster returns the raft cluster servers
-	Cluster() (RaftCluster, error)
-
-	// LeadServer returns the raft lead server
-	LeadServer() (Peer, error)
-
-	// WaitForLeader blocks until a leader is detected, or the timeout expires.
-	WaitForLeader(timeout time.Duration) (string, error)
-
-	// LeaderCh is used to get a channel which delivers signals on
-	// acquiring or losing leadership. It sends true if we become
-	// the leader, and false if we lose it. The channel is not buffered,
-	// and does not block on writes.
-	LeaderCh() <-chan bool
-
-	// NodeState returns the state of current node
-	NodeState() string
 }
 
 // Command defines raft log value's structure
